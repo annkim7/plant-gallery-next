aa2c129e5b90b390ebef8a556837f0004ad5bace
annkim7 <ilc7748@gmail.com>
2023-06-21 20:23:47 +0900

    [Refact] #25 팝업 추가

--- app/mypage/MyInfo.tsx
+++ app/mypage/MyInfo.tsx
@@ -4,40 +4,46 @@ import Img from '@/components/Img'
 import Link from 'next/link'
 import { useDeleteMember } from '@/hook/member'
 import { useSession } from 'next-auth/react'
+import Modal from '@/components/Modal'
+import useModal from '@/hook/modal'
 
 export default function MyInfo() {
   const { data: session } = useSession()
   const { mutate } = useDeleteMember()
+  const [modal, handleModal] = useModal()
 
   const handleDelete = () => {
     mutate(session?.user?.email)
   }
 
   return (
-    <div>
-      <Img
-        width="16"
-        height="16"
-        src={session?.user?.image}
-        alt={session?.user?.name}
-      />
+    <>
       <div>
-        <strong>닉네임</strong>
-        <span>{session?.user?.name}</span>
-      </div>
-      <div>
-        <strong>이메일</strong>
-        <span>{session?.user?.email}</span>
-      </div>
+        <Img
+          width="16"
+          height="16"
+          src={session?.user?.image}
+          alt={session?.user?.name}
+        />
+        <div>
+          <strong>닉네임</strong>
+          <span>{session?.user?.name}</span>
+        </div>
+        <div>
+          <strong>이메일</strong>
+          <span>{session?.user?.email}</span>
+        </div>
 
-      <div>
-        <button type="button">
-          <Link href={`/userInfo/${session?.user?.email}`}>수정</Link>
-        </button>
-        <button type="button" onClick={handleDelete}>
-          탈퇴
-        </button>
+        <div>
+          <button type="button">
+            <Link href={`/userInfo/${session?.user?.email}`}>수정</Link>
+          </button>
+          <button type="button" onClick={handleModal}>
+            탈퇴
+          </button>
+        </div>
       </div>
-    </div>
+      {modal && <Modal func={handleModal} action={handleDelete} />}
+    </>
   )
 }
