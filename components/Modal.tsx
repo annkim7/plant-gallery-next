a9be15fea5495d8b189ad2bcbf4979a2ba9cd035
annkim7 <ilc7748@gmail.com>
2023-06-27 20:09:25 +0900

    [Design] #28 모달 디자인 수정

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -8,11 +8,10 @@ interface ModalProps {
 
 export default function Modal({ func, action }: ModalProps) {
   return (
-    <div className="fixed left-0 top-0 z-[1055] h-full w-full overflow-y-auto overflow-x-hidden outline-none">
+    <div className="fixed left-0 top-0 z-[1055] h-full w-full overflow-y-auto overflow-x-hidden outline-none bg-black bg-opacity-40">
       <div className="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]">
         <div className="pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
-          <div className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-            
+          <div className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 px-4 py-2 dark:border-opacity-50">
             <Label label="회원 탈퇴" />
 
             <button
@@ -37,12 +36,12 @@ export default function Modal({ func, action }: ModalProps) {
             </button>
           </div>
 
-          <div className="relative p-4">
-            <p>탈퇴하시겠습니까?</p>
+          <div className="relative px-6 py-2">
+            <p className="text-slate-500">탈퇴하시겠습니까?</p>
           </div>
 
           <div className="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
-            <Button label="탈퇴" func={action} style="border" />
+            <Button label="탈퇴" func={action} style="square" />
           </div>
         </div>
       </div>
